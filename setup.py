255b1d9227f5f0fd230ab8e2d0ec6884b93b5dc7
Chris Sewell <chrisj_sewell@hotmail.com>
2019-08-07 03:43:27 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-notebook',
+    version='0.1.0',
+    author='Chris Sewell',
+    author_email='chrisj_sewell@hotmail.com',
+    maintainer='Chris Sewell',
+    maintainer_email='chrisj_sewell@hotmail.com',
+    license='BSD-3',
+    url='https://github.com/chrisjsewell/pytest-notebook',
+    description='A simple plugin to use with pytest',
+    long_description=read('README.rst'),
+    py_modules=['pytest_notebook'],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['pytest>=3.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],
+    entry_points={
+        'pytest11': [
+            'notebook = pytest_notebook',
+        ],
+    },
+)
