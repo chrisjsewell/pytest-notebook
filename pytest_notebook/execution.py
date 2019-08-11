ddfc5607d2a193fa3e7a6538d3451ce5765d37fa
Chris Sewell <chrisj_sewell@hotmail.com>
2019-08-11 16:57:54 +0100

    add cell execution logging

--- pytest_notebook/execution.py
+++ pytest_notebook/execution.py
@@ -1,11 +1,46 @@
 """Execution of notebooks."""
 import copy
+import logging
 import shutil
 import tempfile
 from typing import Tuple, Union
 
 from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
 from nbformat import NotebookNode
+
+logger = logging.getLogger(__name__)
+
+
+class ExecutePreprocessorLogging(ExecutePreprocessor):
+    """A subclass of ``nbconvert.ExecutePreprocessor`` that logs per cell execution."""
+
+    def preprocess_cell(self, cell, resources, cell_index):
+        """Execute a single code cell.
+
+        See base.py for details.
+
+        To execute all cells see :meth:`preprocess`.
+        """
+        if cell.cell_type != "code" or not cell.source.strip():
+            return cell, resources
+
+        logger.info(f"Executing cell {cell_index}")
+
+        reply, outputs = self.run_cell(cell, cell_index)
+        # Backwards compatability for processes that wrap run_cell
+        cell.outputs = outputs
+
+        cell_allows_errors = (
+            self.allow_errors or "raises-exception" in cell.metadata.get("tags", [])
+        )
+
+        if self.force_raise_errors or not cell_allows_errors:
+            for out in cell.outputs:
+                if out.output_type == "error":
+                    raise CellExecutionError.from_cell_and_msg(cell, out)
+            if (reply is not None) and reply["content"]["status"] == "error":
+                raise CellExecutionError.from_cell_and_msg(cell, reply["content"])
+        return cell, resources
 
 
 def execute_notebook(
@@ -26,7 +61,8 @@ def execute_notebook(
 
     """
     new_notebook = copy.deepcopy(notebook)
-    proc = ExecutePreprocessor(timeout=timeout, allow_errors=allow_errors)
+    proc = ExecutePreprocessorLogging(timeout=timeout, allow_errors=allow_errors)
+
     if not cwd:
         cwd_dir = tempfile.mkdtemp()
     resources = {
